5651c36c249fbb886511ace6e9216a114fe4d1cd
yus003473-dev <yus003473@gmail.com>
2025-12-26 15:36:28 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,16 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  // 如果你的 GitHub 仓库名是 'my-repo'，base 应该设置为 '/my-repo/'
+  // 如果是个人主页根目录，请设置为 '/'
+  base: './', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+  }
 });
